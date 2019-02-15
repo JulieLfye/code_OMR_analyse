6e18746b1551c147ebec6e0e4930aca3704ca3ce
Julie Lafaye <julie.lafaye07@gmail.com>
2019-02-15 17:09:49 +0100

    update

--- extract_parameters_from_fast_track.m
+++ extract_parameters_from_fast_track.m
@@ -1,5 +1,18 @@
-function [nb_frame, nb_detected_object, xbody, ybody]...
+function [nb_tracked_object, nb_frame, nb_detected_object, xbody, ybody]...
     = extract_parameters_from_fast_track(s)
+
+% determine number of tracked object
+n = 0;
+i = 1;
+nb_tracked_object = 0;
+while n~=1
+    if s(i,1) == 0
+        nb_tracked_object = nb_tracked_object + 1;
+        i = i+1;
+    else
+        n = 1;
+    end
+end
 
 % determine number of frame
 nb_frame = max(s(:,end));
--- test.m
+++ test.m
@@ -1,1 +1,10 @@
 % code test
+
+disp('Select the first frame of the binarized movie');
+[file,path] = uigetfile('*.tif',[],'C:\Users\LJP\Documents\MATLAB\these\data_OMR\');
+% [file,path] = uigetfile('*.tif',[],'G:\these\pot_vibrant');
+t = readtable(fullfile(path,'Tracking_Result\tracking.txt'),'Delimiter','\t');
+s = table2array(t);
+
+[nb_tracked_object, nb_frame, nb_detected_object, xbody, ybody]...
+    = extract_parameters_from_fast_track(s);
